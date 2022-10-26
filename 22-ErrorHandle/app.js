742444fe8be35aeb75321a4b2d720a22ee79e2b1
Leegunhee777 <akanfldkdlel@ajou.ac.kr>
2022-10-27 02:58:20 +0900

    feat: 동기 비동기 error handle

--- 22-ErrorHandle/app.js
+++ 22-ErrorHandle/app.js
@@ -7,16 +7,39 @@ const app = express();
 app.use(express.json());
 
 app.get('/file1', (req, res) => {
-  const data = fs.readFileSync('/file.txt');
-  // fs.readFile('/file1.txt', (err, data) => {});
+  //비동기 error handle
+  fs.readFile('/file1.txt', (err, data) => {
+    if (err) {
+      res.status(404).send('File not found');
+    }
+  });
+
+  //동기 error handle
+  // try {
+  //   const data = fs.readFileSync('/file.txt');
+  // } catch (error) {
+  //   res.status(404).send('File not found');
+  // }
 });
 
-app.get('/file2', (req, res) => {
-  fsAsync.readFile('/file.txt');
+app.get('/file2', (req, res, next) => {
+  //Promise 방식  == 비동기 방식
+
+  fsAsync
+    .readFile('/file.txt')
+    .then(data => {})
+    .catch(next);
+
+  //tip 전달인자와 호출인자가 같을 경우엔 생략가능
 });
 
 app.get('/file3', async (req, res) => {
-  const data = await fsAsync.readFile('/file.txt');
+  //await를 했기때문에 fsAsync임에도 불구하고 동기적이다!!!
+  try {
+    const data = await fsAsync.readFile('/file.txt');
+  } catch (error) {
+    res.status(404).send('File not found');
+  }
 });
 
 app.use((error, req, res, next) => {
